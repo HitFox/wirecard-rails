efab6f0b468b3ff9f79e79b54520693d1961877d
itschn <lofd23@yahoo.de>
2015-07-28 17:39:10 +0200

    Refactoring file structure

--- lib/generators/wirecard/config_generator.rb
+++ lib/generators/wirecard/config_generator.rb
@@ -1,12 +1,10 @@
 module Wirecard
-  module Rails
-    module Generators
-      class ConfigGenerator < Rails::Generators::Base
-        source_root File.expand_path("../templates", __FILE__)
-      
-        def copy_initializer_file
-          copy_file "initializer.rb", "config/initializers/wirecard.rb"
-        end
+  module Generators
+    class ConfigGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+    
+      def copy_initializer_file
+        copy_file "initializer.rb", "config/initializers/wirecard.rb"
       end
     end
   end
